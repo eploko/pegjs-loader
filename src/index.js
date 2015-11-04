82352497bde7e8300029a2e2707c0b4333e55658
Andrey Subbotin <andrey@subbotin.me>
2015-11-04 10:59:52 +0300

    Convert sources to ES6

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import pegjs from 'pegjs';
+import loaderUtils from 'loader-utils';
+
+export default (source) => {
+  if (this.cacheable) {
+    this.cacheable();
+  }
+
+  const query = loaderUtils.parseQuery(this.query);
+  const cacheParserResults = !!query.cache;
+
+  // Description of PEG.js options: https://github.com/pegjs/pegjs#javascript-api
+  const pegOptions = { output: 'source', cache: cacheParserResults };
+
+  return `module.exports = ${pegjs.buildParser(source, pegOptions)};`;
+};
